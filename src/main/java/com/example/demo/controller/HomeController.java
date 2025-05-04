48e89837aacf6ee745d4ec8f5efa930bc0e4c5e7
Gabriel <gabrielsaunders410@gmail.com>
2025-05-04 22:18:01 +0100

    First commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"","/","/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Bobb Doe");
+        return "home.html";
+    }
+
+}
